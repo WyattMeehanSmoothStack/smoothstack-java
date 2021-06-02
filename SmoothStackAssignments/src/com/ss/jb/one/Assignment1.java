f045d5d0844d46f2aac7485534b226c5ebc5240d
meeha <meeha@192.168.1.116>
2021-06-02 12:29:54 -0400

    Formatting

--- SmoothStackAssignments/src/com/ss/jb/one/Assignment1.java
+++ SmoothStackAssignments/src/com/ss/jb/one/Assignment1.java
@@ -2,13 +2,14 @@ package com.ss.jb.one;
 
 /**
  * This class is for Java Basics Assignment 1
+ * 
  * @author Wyatt Meehan
  */
 
 public class Assignment1 {
-	
-    static String pattern1 = "1)\n" + "*\n" + "**\n" + "***\n" + "****\n" + ".........\n";
-    static String pattern2 = "2)\n" + "..........\n" + "****\n" + "***\n" + "**\n" + "*\n";
+
+	static String pattern1 = "1)\n" + "*\n" + "**\n" + "***\n" + "****\n" + ".........\n";
+	static String pattern2 = "2)\n" + "..........\n" + "****\n" + "***\n" + "**\n" + "*\n";
 	static String pattern3 = "3)\n" + "     *\n" + "    ***\n" + "   *****\n" + "  *******\n" + "...........\n";
 	static String pattern4 = "4)\n" + "............\n" + "  *******\n" + "   *****\n" + "    ***\n" + "     *\n";
 
