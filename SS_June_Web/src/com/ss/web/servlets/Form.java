25411573c4288f3edfcf6275258d88abb625d975
Wyatt Meehan <wyatt.meehan@smoothstack.com>
2021-06-09 12:51:45 -0400

    Maven and Form Servlet assignment

--- SS_June_Web/src/com/ss/web/servlets/Form.java
+++ SS_June_Web/src/com/ss/web/servlets/Form.java
@@ -35,8 +35,35 @@ public class Form extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+        // read form fields
+		String correctUsername = "Wyatt";
+		String correctPassword = "Password";
+		
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+         
+        System.out.println("User entered " + username + "for username");
+        System.out.println("User entered " + password + "for password");
+         
+        PrintWriter writer = response.getWriter();
+        
+        String correct = "";
+        if(correctUsername.equals(username) && correctPassword.equals(password)) {
+        	correct += "<h1> Login successful, username and password were a match!</h1>";
+        } else {
+        	correct += "<h1> Login failed, wrong username or password.</h1>";
+        }
+         
+        // build HTML code
+        String htmlRespone = "<html>";
+        htmlRespone += "<p>We would normally validate the username and password with our database but instead I stored then in the servlet. "
+        		+ "The correct username is \"Wyatt\" and the Password is \"Password\"<br/><br/></p>";   
+        htmlRespone += "<h2>You entered username: " + username + "<br/>";      
+        htmlRespone += "You entered password: " + password + "</h2>";
+         
+        // return response
+        writer.println(htmlRespone);
+        writer.println(correct);
 	}
 
 }
