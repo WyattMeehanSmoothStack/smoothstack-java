508e4fb6d6f0404aac580f63908d4367a0eefa71
WyattMeehan2 <wyattmeehan1@gmail.com>
2021-07-13 12:14:06 -0400

    Fixed to use loops

--- SmoothStackAssignments/src/com/ss/jb/one/Assignment1.java
+++ SmoothStackAssignments/src/com/ss/jb/one/Assignment1.java
@@ -1,20 +1,57 @@
 package com.ss.jb.one;
 
 /**
- * This class is for Java Basics Assignment 1
+ * This class is for Java Basics Assignment 1. It uses loops to print various
+ * patterns.
  * 
  * @author Wyatt Meehan
  */
 
 public class Assignment1 {
 
-	static String pattern1 = "1)\n" + "*\n" + "**\n" + "***\n" + "****\n" + ".........\n";
-	static String pattern2 = "2)\n" + "..........\n" + "****\n" + "***\n" + "**\n" + "*\n";
-	static String pattern3 = "3)\n" + "     *\n" + "    ***\n" + "   *****\n" + "  *******\n" + "...........\n";
-	static String pattern4 = "4)\n" + "............\n" + "  *******\n" + "   *****\n" + "    ***\n" + "     *\n";
-
 	public static void main(String[] args) {
-		System.out.println(pattern1 + pattern2 + pattern3 + pattern4);
+		printPattern1();
+		printPattern2();
+		printPattern3();
+		printPattern4();
+	}
+
+	private static void printPattern4() {
+		String pat4 = "***********";
+		System.out.println("\n4)");
+		System.out.println(pat4);
+		while (!pat4.equals("     *")) {
+			int first = pat4.indexOf("*");
+			System.out.println(pat4 = " " + pat4.substring(0, first) + pat4.substring(first + 1, pat4.length() - 1));
+		}
+
+	}
+
+	private static void printPattern3() {
+		String pat3 = "     *     ";
+		System.out.println("\n3)");
+		System.out.println(pat3);
+		while (!pat3.equals("***********")) {
+			int first = pat3.indexOf("*");
+			System.out.println(pat3 = pat3.substring(0, first - 1) + "**" + pat3.substring(first, pat3.length() - 1));
+		}
+	}
+
+	private static void printPattern2() {
+		String pat2 = "***********";
+		System.out.println("\n2)");
+		while (!pat2.equals("*")) {
+			System.out.println(pat2 = pat2.substring(0, pat2.length() - 1));
+		}
+
+	}
+
+	public static void printPattern1() {
+		String pat1 = "";
+		System.out.println("1)");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(pat1 += "*");
+		}
 	}
 
 }
