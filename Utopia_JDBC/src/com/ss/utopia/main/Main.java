518a3d59a993209fde214f78cd2b21f23c375115
WyattMeehan2 <wyattmeehan1@gmail.com>
2021-07-27 14:12:29 -0400

    Test Commit

--- Utopia_JDBC/src/com/ss/utopia/main/Main.java
+++ Utopia_JDBC/src/com/ss/utopia/main/Main.java
@@ -19,6 +19,7 @@ public class Main {
 	 */
 	public static void main(String[] args) throws SQLException {
 		mainMenu();
+		System.out.println();
 	}
 
 	public static void mainMenu() throws SQLException {
